7ac917aa922371893e6b9fd16e2be32b0e2a2c27
drivesalem307 <drivexodus93@gmail.com>
2026-08-20 10:37:43 +0300

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = java.net.URI("https://jitpack.io") }
+    }
+}
+rootProject.name = "RunnerCloudStream"
